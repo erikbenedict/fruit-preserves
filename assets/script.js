caddbb447c13eb09a6ff4fdd07c71d5c24fcb6fa
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-04-27 21:53:53 -0400

    deleted mysecrets.js file; deleted .gitignore file; changed clientID && clientSecret to local variables; added function to get access token for spotify api

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,56 @@
-import secrets from '../mysecrets.js';
-const clientId = secrets.clientId
-const clientSecret = secrets.clientSecret
+const clientId = '463e1867583147f78d5b25bcc0004dda';
+const clientSecret = '3e7065f63a874ad5a2c715aa6c24aed9';
 const searchedResults = document.getElementById('searchedResults')
 const srcForm = document.getElementById('searchForm');
 const srcBar = document.getElementById('srcInput');
 const prevSrcs = document.getElementById('previousSearches');
 let srcHistoryItems = [];
+
+// * create access token to use for API requests *
+const authOptions = {
+	method: 'POST',
+	headers: {
+	  'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
+	  'Content-Type': 'application/x-www-form-urlencoded'
+	},
+	body: 'grant_type=client_credentials'
+  };
+  
+  fetch('https://accounts.spotify.com/api/token', authOptions)
+    .then(response => response.json())
+    .then(data => {
+        const token = data.access_token;
+	// * API request test *
+	// 	const trackOptions = {
+	// 	headers: {
+	// 	'Authorization': 'Bearer ' + token
+	// 	}
+	// };
+
+	// fetch('https://api.spotify.com/v1/tracks/6QjmnLmLqwjJzHSDCRTACU', trackOptions)
+	// 	.then(response => response.json())
+	// 	.then(data => {
+	// 	console.log(data);
+	// 	const sampleUl = document.createElement('ul');
+	// 	const sample = document.createElement('li');
+	// 	const audio = document.createElement('audio');
+	// 	const trackName = document.createElement('div');
+	// 	const artistName = document.createElement('div');
+	// 	const albumName = document.createElement('div');
+
+	// 	audio.setAttribute('controls', true);
+	// 	audio.setAttribute('src', data.preview_url);
+
+	// 	trackName.innerHTML = `Track Name: ${data.name}`;
+	// 	artistName.innerHTML = `Artist Name: ${data.artists[0].name}`;
+	// 	albumName.innerHTML = `Album Name: ${data.album.name}`;
+
+	// 	sample.append(audio, trackName, artistName, albumName);
+	// 	sampleUl.append(sample);
+	// 	searchedResults.append(sampleUl);
+	// 	})
+	// 	.catch(error => console.error(error));
+	});
 
 function renderSrcHistoryItems() {
 prevSrcs.innerHTML = "";
@@ -27,79 +72,29 @@ function loadSavedSrcs() {
   renderSrcHistoryItems();
 }
 
-const authOptions = {
-	method: 'POST',
-	headers: {
-	  'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
-	  'Content-Type': 'application/x-www-form-urlencoded'
-	},
-	body: 'grant_type=client_credentials'
-  };
-  
-  fetch('https://accounts.spotify.com/api/token', authOptions)
-    .then(response => response.json())
-    .then(data => {
-		console.log(data);
-        const token = data.access_token;
-
-    // make API call to get track data using the token
-    const trackOptions = {
-      headers: {
-        'Authorization': 'Bearer ' + token
-      }
-    };
-
-    fetch('https://api.spotify.com/v1/tracks/6QjmnLmLqwjJzHSDCRTACU', trackOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-	// 	const audioElement = document.createElement('audio');
-    // audioElement.setAttribute('src', data.preview_url);
-    // audioElement.setAttribute('controls', true);
-    // searchedResults.appendChild(audioElement);
-    //   })
-    //   .catch(error => console.error(error));
-	const sampleUl = document.createElement('ul');
-    const sample = document.createElement('li');
-    const audio = document.createElement('audio');
-    const trackName = document.createElement('div');
-    const artistName = document.createElement('div');
-    const albumName = document.createElement('div');
-
-    audio.setAttribute('controls', true);
-    audio.setAttribute('src', data.preview_url);
-
-    trackName.innerHTML = `Track Name: ${data.name}`;
-    artistName.innerHTML = `Artist Name: ${data.artists[0].name}`;
-    albumName.innerHTML = `Album Name: ${data.album.name}`;
-
-    sample.append(audio, trackName, artistName, albumName);
-    sampleUl.append(sample);
-    searchedResults.append(sampleUl);
-  })
-  .catch(error => console.error(error));
-});
 function storeSearches() {
-  localStorage.setItem("localStoredSrcs", JSON.stringify(srcHistoryItems));
-}
-srcForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  let srcText = srcBar.value.trim();
-  if (srcText === "") {
-    return;
+	localStorage.setItem("localStoredSrcs", JSON.stringify(srcHistoryItems));
   }
-  srcHistoryItems.push(srcText);
-  srcBar.value = "";
- 
-  storeSearches();
-  renderSrcHistoryItems();
-});
+  srcForm.addEventListener("submit", function(event) {
+	event.preventDefault();
+	let srcText = srcBar.value.trim();
+	if (srcText === "") {
+	  return;
+	}
+	srcHistoryItems.push(srcText);
+	srcBar.value = "";
+   
+	storeSearches();
+	renderSrcHistoryItems();
+  });
+  
+  const clearSrcHistoryButton = document.getElementById('clearSrcHistory')
+  clearSrcHistoryButton.addEventListener('click', function(){
+	  prevSrcs.innerHTML = "";
+	  srcHistoryItems = [];
+	  localStorage.clear();
+  });
+  console.log(localStorage);
 
-const clearSrcHistoryButton = document.getElementById('clearSrcHistory')
-clearSrcHistoryButton.addEventListener('click', function(){
-    prevSrcs.innerHTML = "";
-    srcHistoryItems = [];
-    localStorage.clear();
-});
+  
 
-console.log(localStorage);
