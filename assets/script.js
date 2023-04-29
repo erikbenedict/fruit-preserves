585e81d59140d5dbda2aa7b7a8cefc1c7569800c
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-04-29 11:19:31 -0400

    chore: made rendered search history items display as buttons; styled searchHistory div for better readability

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,129 @@
 const clientId = '463e1867583147f78d5b25bcc0004dda';
 const clientSecret = '3e7065f63a874ad5a2c715aa6c24aed9';
-const searchedResults = document.getElementById('searchedResults')
+const searchedResults = document.getElementById('searchedResults');
+const suggestions = document.getElementById('suggestions')
 const searchForm = document.getElementById('searchForm');
-const searchBar = document.getElementById('srcInput');
+const searchInput = document.getElementById('srcInput');
+const searchType = document.getElementById('searchType');
 const prevSearches = document.getElementById('previousSearches');
 let searchHistoryItems = [];
 
-// * create access token to use for API requests *
-const authOptions = {
-	method: 'POST',
-	headers: {
-	  'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
-	  'Content-Type': 'application/x-www-form-urlencoded'
-	},
-	body: 'grant_type=client_credentials'
-  };
-  
-  fetch('https://accounts.spotify.com/api/token', authOptions)
-    .then(response => response.json())
-    .then(data => {
-        const token = data.access_token;
-	// * API request test *
-	// 	const trackOptions = {
-	// 	headers: {
-	// 	'Authorization': 'Bearer ' + token
-	// 	}
-	// };
+// * function to create access token to use for API requests *
+async function getAccessToken() {
+	try { 
+		const authOptions = {
+			method: 'POST',
+			headers: {
+			'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
+			'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: 'grant_type=client_credentials'
+		};
+		const response = await fetch('https://accounts.spotify.com/api/token', authOptions);
+		const data_1 = await response.json();
+		const token = data_1.access_token;
+		return token;
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-	// fetch('https://api.spotify.com/v1/tracks/6QjmnLmLqwjJzHSDCRTACU', trackOptions)
-	// 	.then(response => response.json())
-	// 	.then(data => {
-	// 	console.log(data);
-	// 	const sampleUl = document.createElement('ul');
-	// 	const sample = document.createElement('li');
-	// 	const audio = document.createElement('audio');
-	// 	const trackName = document.createElement('div');
-	// 	const artistName = document.createElement('div');
-	// 	const albumName = document.createElement('div');
+// * Event listener for submit on searchForm *
+searchForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  let textInput = searchInput.value.trim();
+  let type = searchType.value.trim();
+  let searchText = `${textInput} : ${type}`;
+  if (searchText === "") {
+    return;
+  }
+  searchHistoryItems.push(searchText);
+  searchInput.value = "";
+  storeSearches();
+  renderSearchHistoryItems();
 
-	// 	audio.setAttribute('controls', true);
-	// 	audio.setAttribute('src', data.preview_url);
+//   * creates endpoints and seed types based on selection in searchForm *
+  let seedType;
+  let endpoint;
+  if (type === 'Artist') {
+    endpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(textInput)}&type=artist&limit=1`;
+    seedType = 'seed_artists';
+  } else if (type === 'Song') {
+    endpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(textInput)}&type=track&limit=1`;
+    seedType = 'seed_tracks';
+  } else {
+    endpoint = `https://api.spotify.com/v1/recommendations?limit=1&market=US&seed_genres=${encodeURIComponent(textInput)}`;
+    seedType = 'seed_genres';
+  }
 
-	// 	trackName.innerHTML = `Track Name: ${data.name}`;
-	// 	artistName.innerHTML = `Artist Name: ${data.artists[0].name}`;
-	// 	albumName.innerHTML = `Album Name: ${data.album.name}`;
+  const token = await getAccessToken();
 
-	// 	sample.append(audio, trackName, artistName, albumName);
-	// 	sampleUl.append(sample);
-	// 	searchedResults.append(sampleUl);
-	// 	})
-	// 	.catch(error => console.error(error));
-	});
+  const response = await fetch(endpoint, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
 
-function renderSearcHistoryItems() {
+//   ! >>>>>> data >>>>> !
+  console.log('>>>>>> data >>>>>', data);
+
+//   * seed track path based on selection in searchForm *
+  let seedTrack;
+  if (type === 'Song') {
+    seedTrack = data.tracks.items[0].id;
+  } else if (type === 'Artist') {
+	seedTrack = data.artists.items[0].id
+  } else {
+    seedTrack = data.seeds[0].id
+  }
+
+//   * Endpoint for User search to create recommended songs *
+  const recommendationsEndpoint = `https://api.spotify.com/v1/recommendations?market=US&${seedType}=${seedTrack}`;
+
+  const recommendationsResponse = await fetch(recommendationsEndpoint, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const recommendationsData = await recommendationsResponse.json();
+//    ! >>>>>>>>> recommendations data >>>>>>> !
+  console.log('>>>>> recommendationsData >>>>', recommendationsData);
+
+//   * Display recommended track in the searchedResults div *
+	//  * clears previous suggestions
+  suggestions.innerHTML = '';
+	// * Loop through the tracks in the recommendations data
+  let recommendationCount = 0;
+  for (let i = 0; i < recommendationsData.tracks.length && recommendationCount < 5; i++) {
+  	// * creates new suggestion elements and adds them to the recommendation count
+  const songSuggestion = document.createElement('div');
+  songSuggestion.setAttribute('class', 'col-12 bg-gray-600 text-white my-2');
+let iframe = document.createElement('iframe');
+  iframe.setAttribute('src', `https://open.spotify.com/embed/track/${recommendationsData.tracks[i].id}`);
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', '80');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allowtransparency', 'true');
+  iframe.setAttribute('allow', 'encrypted-media');
+  songSuggestion.append(iframe);
+  suggestions.append(songSuggestion);
+
+  recommendationCount++;
+}
+});
+
+function renderSearchHistoryItems() {
 prevSearches.innerHTML = "";
   for (let i = 0; i < searchHistoryItems.length; i++) {
     let searchHistoryItem = searchHistoryItems[i];
     let searchHistoryLine = document.createElement('div');
-    searchHistoryLine.textContent = searchHistoryItem;
-    prevSearches.appendChild(searchHistoryLine);
+	let searchHistoryBtn = document.createElement('button');
+	searchHistoryBtn.setAttribute('class', 'bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-1');
+	searchHistoryBtn.setAttribute('id', 'searchHistoryBtn');
+    searchHistoryBtn.textContent = searchHistoryItem;
+	searchHistoryLine.append(searchHistoryBtn);
+    prevSearches.append(searchHistoryLine);
   };
   if(searchHistoryItems.length === 5){
     searchHistoryItems.shift();
@@ -66,28 +131,16 @@ prevSearches.innerHTML = "";
 }
 
 function loadSavedSearches() {
-  var storedSearches = JSON.parse(localStorage.getItem("localStoredSearches"));
+  let storedSearches = JSON.parse(localStorage.getItem("localStoredSearches"));
   if (storedSearches !== null) {
     searchHistoryItems = storedSearches;
   }
-  renderSearcHistoryItems();
+  renderSearchHistoryItems();
 }
 
 function storeSearches() {
   localStorage.setItem("localStoredSearches", JSON.stringify(searchHistoryItems));
 }
-searchForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  let searchText = searchBar.value.trim();
-  if (searchText === "") {
-    return;
-  }
-  searchHistoryItems.push(searchText);
-  searchBar.value = "";
-  storeSearches();
-  renderSearcHistoryItems();
-});
-
 
 const clearSearchHistoryButton = document.getElementById('clearSrcHistory')
 clearSearchHistoryButton.addEventListener('click', function(){
